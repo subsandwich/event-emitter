fc41de296c93c8efbb3dd5b0d3978a7d8586f966
jake612 <jake612@live.unc.edu>
2023-04-23 15:04:02 -0400

    Working hello world example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { EventBridgeEvent } from 'aws-lambda';
 /**
  * A Lambda function that logs the payload received from a CloudWatch scheduled event.
  */
-export const lambdaHandler = async (event: EventBridgeEvent<'Scheduled Event', any>): Promise<void> => {
+export const lambdaHandler = async (event: any): Promise<void> => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.info(JSON.stringify(event));
